2ef84007ea1033711eb8f60193db18323fd84335
Joseph NG <32561250+JosephNGala@users.noreply.github.com>
2021-06-01 07:18:37 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,10 @@
+
+// Week 6 Group Project: Mixed Messages by tinisha, oscar, michelle, julio, imani
+
+const suspectedElements = {
+    whoDoneIt: ['Miss Scarlett', 'Professor Plum', 'Mrs. Peacock', 'Colonel Mustard', 'Mrs. White', 'Mr. Green'],
+    whereItDone: ['ballroom', 'billiard room', 'conservatory', 'dining room', 'hall', 'kitchen', 'library', 'lounge', 'study'],
+    whatDoneIt: ['candlestick', 'knife', 'lead pipe', 'revolver', 'rope', 'wrench']
+};
+
+console.log(suspectedElements[5]);
